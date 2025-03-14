15fa0064304cb710748d6ff950aada6edf20b716
Melksedeque Silva <freelancer@melksedeque.com.br>
2025-03-14 13:17:29 -0300

    Criação de rotas de feira e carrinho importando as paginas

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
+import Carrinho from "pages/Carrinho";
+import Feira from "pages/Feira";
 import Login from "pages/Login";
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
@@ -7,6 +9,12 @@ function Router() {
             <Switch>
                 <Route exact path="/">
                     <Login />
+                </Route>
+                <Route path="/feira">
+                    <Feira />
+                </Route>
+                <Route path="/carrinho">
+                    <Carrinho />
                 </Route>
             </Switch>
         </BrowserRouter>
