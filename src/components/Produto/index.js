310e5b9382e1a4f9404ab4fb1d77453e5f868531
Melksedeque Silva <freelancer@melksedeque.com.br>
2025-03-27 10:52:42 -0300

    Importação e uso do hook useCarrinhoContext no lugar do useContext

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,9 +1,9 @@
 import { Container } from './styles';
-import { memo, useContext } from 'react';
+import { memo } from 'react';
 import { IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
-import { CarrinhoContext } from 'common/context/Carrinho';
+import { useCarrinhoContext } from 'common/context/Carrinho';
 
 function Produto({
   nome,
@@ -12,7 +12,7 @@ function Produto({
   valor,
   unidade
 }) {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext)
+  const { carrinho, setCarrinho } = useCarrinhoContext()
   function adicionarProduto(novoProduto) {
     const temNoCarrinho = carrinho.some(item => item.id === novoProduto.id);
     if(!temNoCarrinho){
